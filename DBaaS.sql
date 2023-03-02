1c4c15c6ef7f14e6d656b0e1205300cf0143ed88
Mohammad Mahdi Nemati <nemati.mahdi255@gmail.com>
2023-03-02 20:13:15 +0330

    initial

---
+++ DBaaS.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS DBaaS;
+CREATE DATABASE DBaaS; 
+USE DBaas;
+
+SET NAMES utf8mb4 ;
+SET character_set_client = utf8mb4 ;
+
+
+CREATE TABLE uploads (
+  id 				int NOT NULL AUTO_INCREMENT,
+  email 			varchar(320) NOT NULL,
+  inputs 			varchar(512),
+  program_language  ENUM('py', 'cpp', 'c', 'java') NOT NULL,
+  is_enable 	 	bool NOT NULL DEFAULT FALSE, 
+  PRIMARY KEY (id)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE results (
+  id 				int NOT NULL AUTO_INCREMENT,
+  job 				varchar(320) NOT NULL,
+  output 			varchar(1024),
+  execute_status  	ENUM('in progress', 'done') NOT NULL,
+  execute_date 	 	datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+  PRIMARY KEY (id),
+  FOREIGN KEY (job) REFERENCES jobs(id)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE jobs (
+  id 				int NOT NULL AUTO_INCREMENT,
+  upload 			varchar(320) NOT NULL,
+  job 				varchar(512),
+  job_status  		ENUM('executed', 'none') NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (upload) REFERENCES uploads(id)
+) AUTO_INCREMENT=1;
